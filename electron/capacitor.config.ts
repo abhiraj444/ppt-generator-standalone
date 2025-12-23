1754f5039047c3e8d56fa235db70fa5acd1b482d
abhiraj444 <ahirajpm143@gmail.com>
2025-12-23 21:57:26 +0530

    next try

--- electron/capacitor.config.ts
+++ electron/capacitor.config.ts
@@ -3,7 +3,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
     appId: 'com.medigen.app',
     appName: 'MediGen',
-    webDir: 'app', // In electron, it's usually 'app' because Capacitor copies 'out' to 'electron/app'
+    webDir: 'out',
     server: {
         androidScheme: 'https'
     }
